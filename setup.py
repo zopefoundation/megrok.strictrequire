b1a2541a095f1ca3d6c75cc14a507c033aa6e8dd
Jan-Jaap Driessen <jdriessen@minddistrict.com>
2018-01-10 14:35:01 +0100

    Back to development: 0.8

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os.path
 
-version = '0.7'
+version = '0.8.dev0'
 detailed = open(
     os.path.join('src', 'megrok', 'strictrequire', 'README.txt')).read()
 changes = open('CHANGES.txt').read()
